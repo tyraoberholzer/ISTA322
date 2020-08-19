d3cc1b616ed549e7f85fad48bf4a901d27d54826
Tyra Oberholzer <tyra.n.oberholzer@outlook.com>
2020-08-19 12:12:02 -0400

    updated files

---
+++ ASP.NET/Labs/Lab Work/PartyInvites/Models/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>(); //field
+        public static IEnumerable<GuestResponse> Responses => responses;//property
+        public static void AddResponse(GuestResponse response)//method
+        {
+            responses.Add(response);//add a response to the end of the list 
+        }
+    }
+}
