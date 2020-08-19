f79f695e8c02c89b55eb393219b18fc633512d58
Tyra Oberholzer <tyra.n.oberholzer@outlook.com>
2020-08-19 08:49:46 -0400

    updated files

---
+++ C#/Labs/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+//allow user to pick random number then it prints a hash ladder
+
+namespace Mario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            do
+            {
+                Console.WriteLine("Enter a number between 1 and 8: ");
+                count = int.Parse(Console.ReadLine());
+            } while (count < 0 || count > 8);
+            Mario mario = new Mario();
+            mario.Build(count);
+        }
+    }
+    class Mario
+    {
+        public void Build(int height)
+        {
+            int x = height;
+            while (height > 0)
+            {
+                int length = height - 1;
+                int width = height - 1;
+                while (length > 0)
+                {
+                    Console.Write(" ");
+                    length--;
+                }
+                do
+                {
+                    width++;
+                    Console.Write("#");
+                } while (width < x);
+                height--;
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ C#/Labs/GarbageCollectionDemo/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollectionDemo
+{
+    class Calculator : IDisposable
+    {
+        private bool disposed = false;
+        ~Calculator()
+        {
+            Console.WriteLine("Calculator being finalized");
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            //Console.WriteLine("In dispose method, Calculator being disposed");
+            if (!disposed)
+            {
+                Console.WriteLine("Calculator being disposed");
+            }
+            disposed = true;
+            GC.SuppressFinalize(this);
+        }
+
+        public int Divide(int first, int second)
+        {
+            return first / second;
+        }
+    }
+}
